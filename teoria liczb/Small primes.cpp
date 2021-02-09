1987c80c1e8d7622260bfc49324683744640db65
kemot2k20 <61714891+kemot2k20@users.noreply.github.com>
2021-02-09 15:44:30 +0100

    Create Small primes.cpp

---
+++ teoria liczb/Small primes.cpp
@@ -0,0 +1,11 @@
+vector<int> Primes(int n) {
+	vector<bool> P(n + 1, true);
+	for (int i = 2; i * i <= n; i ++)
+		if (P[i])
+			for (int j = i + i; j <= n; j += i)
+				P[j] = false;
+	
+	vector<int> wyn;
+	for (int i = 2; i <= n; i ++) if (P[i]) wyn.push_back(i);
+	return wyn;
+}
