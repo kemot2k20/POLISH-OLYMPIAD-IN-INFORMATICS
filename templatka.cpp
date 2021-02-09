72695c214984764460cb88af139b8f93669f2bed
kemot2k20 <61714891+kemot2k20@users.noreply.github.com>
2021-02-09 16:03:45 +0100

    Create templatka.cpp

---
+++ templatka.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define all(x) x.begin(), x.end()
+#define sajz(x) (int)x.size()
+#define pb push_back
+#define s second
+#define f first
+using namespace std;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef long double ld;
+typedef long long ll;
+
+void test_case() {
+	
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	for (int i = 1; i <= t; i ++) {
+		test_case();
+	}
+	return 0;
+}
