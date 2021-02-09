03d0e19952b666d5161eae0e6e2aaae005ec0681
kemot2k20 <61714891+kemot2k20@users.noreply.github.com>
2021-02-09 16:14:16 +0100

    Update Small primes.cpp

--- teoria liczb/Small primes.cpp
+++ teoria liczb/Small primes.cpp
@@ -2,7 +2,7 @@ vector<int> Primes(int n) {
 	vector<bool> P(n + 1, true);
 	for (int i = 2; i * i <= n; i ++)
 		if (P[i])
-			for (int j = i + i; j <= n; j += i)
+			for (int j = i * i; j <= n; j += i)
 				P[j] = false;
 	
 	vector<int> wyn;
