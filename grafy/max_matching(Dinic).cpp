ddbd46133f22135c7c6c6bb96068d93f894636c4
Tomasz Zyla <61714891+kemot2k20@users.noreply.github.com>
2023-10-23 20:02:22 +0200

    Create max_matching(Dinic).cpp

---
+++ grafy/max_matching(Dinic).cpp
@@ -0,0 +1,178 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+#define INF 1e9
+#define INFl 1e18
+#define all(x) x.begin(), x.end()
+#define sajz(x) (int)x.size()
+#define pb push_back
+#define s second
+#define f first
+#define yes puts("YES")
+#define no puts("NO")
+#define erase_duplicates(x) {sort(all(x));(x).resize(distance((x).begin(), unique(all(x))));}
+using namespace std;
+//using namespace __gnu_pbds;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+#define int ll
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef long double ld;
+//typedef cc_hash_table<int, int, hash<int>> ht;
+
+using namespace std;
+
+struct FlowEdge {
+    int v, u;
+    long long cap, flow = 0;
+    FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
+};
+
+struct Dinic {
+    const long long flow_inf = 1e18;
+    vector<FlowEdge> edges;
+    vector<vector<int>> adj;
+    int n, m = 0;
+    int s, t;
+    vector<int> level, ptr;
+    queue<int> q;
+
+    Dinic(int n, int s, int t) : n(n), s(s), t(t) {
+        adj.resize(n);
+        level.resize(n);
+        ptr.resize(n);
+    }
+
+    void add_edge(int v, int u, long long cap) {
+        edges.emplace_back(v, u, cap);
+        edges.emplace_back(u, v, 0);
+        adj[v].push_back(m);
+        adj[u].push_back(m + 1);
+        m += 2;
+    }
+
+    bool bfs() {
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            for (int id : adj[v]) {
+                if (edges[id].cap - edges[id].flow < 1)
+                    continue;
+                if (level[edges[id].u] != -1)
+                    continue;
+                level[edges[id].u] = level[v] + 1;
+                q.push(edges[id].u);
+            }
+        }
+        return level[t] != -1;
+    }
+
+    long long dfs(int v, long long pushed) {
+        if (pushed == 0)
+            return 0;
+        if (v == t)
+            return pushed;
+        for (int& cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
+            int id = adj[v][cid];
+            int u = edges[id].u;
+            if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1)
+                continue;
+            long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+            if (tr == 0)
+                continue;
+            edges[id].flow += tr;
+            edges[id ^ 1].flow -= tr;
+            return tr;
+        }
+        return 0;
+    }
+
+    long long flow() {
+        long long f = 0;
+        while (true) {
+            fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            q.push(s);
+            if (!bfs())
+                break;
+            fill(ptr.begin(), ptr.end(), 0);
+            while (long long pushed = dfs(s, flow_inf)) {
+                f += pushed;
+            }
+        }
+        return f;
+    }
+};
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+	cout.tie(0);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+
+	while (t --) {
+		int p, t, s, c;
+		cin >> p >> t >> s >> c;
+
+		int n = p + t;
+		Dinic dinic(n + 2, 0, n + 1);
+
+		vector<pii> a(p), b(t);
+		for (int i = 0; i < p; i ++) cin >> a[i].first >> a[i].second;
+		for (int i = 0; i < t; i ++) cin >> b[i].first >> b[i].second;
+
+		for (int i = 0; i < t; i ++) {
+			for (int j = 0; j < p; j ++) {
+				int odl = abs(a[j].first - b[i].first) + abs(a[j].second - b[i].second);
+				int needT = (200 * odl + s - 1) / s;
+
+				if (needT <= c) {
+					dinic.add_edge(i + 1, j + 1 + t, 1);
+				}
+			}
+		}
+
+		for (int i = 0; i < t; i ++) dinic.add_edge(0, i + 1, 1);
+		for (int j = 0; j < p; j ++) dinic.add_edge(j + 1 + t, n + 1, 1);
+
+		int ans = dinic.flow();
+
+		cout << ans << '\n';
+	}
+
+    return 0;
+}
