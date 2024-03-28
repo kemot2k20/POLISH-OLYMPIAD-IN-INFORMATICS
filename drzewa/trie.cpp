e33d3b2a11c1739f95e4a273523fd358ee94daae
Tomasz Zyla <61714891+kemot2k20@users.noreply.github.com>
2024-03-28 01:10:10 +0100

    Update trie.cpp

--- drzewa/trie.cpp
+++ drzewa/trie.cpp
@@ -1,3 +1,44 @@
+const int K = 19;
+const int N = 2e5 + 5;
+const int MX = N * K;
+
+int t[MX][2];
+int sz = 1;
+
+void init() {
+    sz = 1;
+    for (int i = 0; i < MX; i ++)
+        for (int j = 0; j < 2; j ++) 
+            t[i][j] = -1;
+}
+
+void add(int x) {
+    int v = 0;
+    for (int bit = K - 1; bit >= 0; bit --) {
+        int b = (x >> bit) & 1;
+        if (t[v][b] == -1) {
+            t[v][b] = sz ++;
+        }
+        v = t[v][b];
+    }
+}
+
+int maxi(int x) {
+    int v = 0, res = 0;
+    for (int bit = K - 1; bit >= 0; bit --) {
+        int b = (x >> bit) & 1;
+        if (t[v][b^1] != -1) {
+            res += (1 << bit);
+            b ^= 1;
+        }
+        v = t[v][b];
+    }
+    return res;
+}
+
+
+
+
 vector<array<int, 2>> t({{-1, -1}});
   auto add = [&](int x) {
     int v = 0;
